5020d1627bb653f6e288dbd1a94cc24a23eb1b5d
cdd <cddlyf@gmail.com>
2014-07-25 21:50:15 +0800

    添加了不同类别的酒

--- test/add_wine.js
+++ test/add_wine.js
@@ -1,18 +1,18 @@
 var Wine = require('../models').Wine;
 
   wine = new Wine({
-        id : "fy20140716002",
-        name : "口子窖",
-        describe : "41度口子窖六年真藏实窖450ml",
-        marketPrice : 168,
-        wechatPrice : 128,
-        littlePic : "fy20140716001_s.jpg",
+        id : "fy20140716005",
+        name : "汾酒",
+        describe : "53度汾酒蓝瓷475ml",
+        marketPrice : 118,
+        wechatPrice : 88,
+        littlePic : "fy20140716005_s.jpg",
         details : {
-          degree : 41,
-          volume : 450,
+          degree : 53,
+          volume : 475,
           place : '阜阳'
         },
-        bigPic : ["fy20140716001_l_01.jpg","fy20140716001_l_02.jpg"],
+        bigPic : ["fy20140716005_l_01.jpg","fy20140716005_l_02.jpg"],
         tag : {
           type : "白酒",
           isRecommend : true
@@ -22,18 +22,18 @@ var Wine = require('../models').Wine;
   });
   wine.save();
    wine1 = new Wine({
-        id : "fy20140716003",
-        name : "口子窖",
-        describe : "41度口子窖六年真藏实窖450ml",
-        marketPrice : 168,
-        wechatPrice : 128,
-        littlePic : "fy20140716001_s.jpg",
+        id : "fy20140716006",
+        name : "剑南春",
+        describe : "52度名酒水晶剑南春500ml浓香型白酒",
+        marketPrice : 358,
+        wechatPrice : 308,
+        littlePic : "fy20140716006_s.jpg",
         details : {
-          degree : 41,
-          volume : 450,
+          degree : 52,
+          volume : 500,
           place : '阜阳'
         },
-        bigPic : ["fy20140716001_l_01.jpg","fy20140716001_l_02.jpg"],
+        bigPic : ["fy20140716006_l_01.jpg","fy20140716006_l_02.jpg"],
         tag : {
           type : "白酒",
           isRecommend : true
@@ -43,18 +43,18 @@ var Wine = require('../models').Wine;
   });
   wine1.save();
    wine2 = new Wine({
-        id : "fy20140716004",
-        name : "口子窖",
-        describe : "41度口子窖六年真藏实窖450ml",
-        marketPrice : 168,
-        wechatPrice : 128,
-        littlePic : "fy20140716001_s.jpg",
+        id : "fy20140716007",
+        name : "郎酒",
+        describe : "53度红花郎酒十年陈酿500ml ",
+        marketPrice : 258,
+        wechatPrice : 208,
+        littlePic : "fy20140716007_s.jpg",
         details : {
-          degree : 41,
-          volume : 450,
+          degree : 53,
+          volume : 500,
           place : '阜阳'
         },
-        bigPic : ["fy20140716001_l_01.jpg","fy20140716001_l_02.jpg"],
+        bigPic : ["fy20140716007_l_01.jpg","fy20140716007_l_02.jpg"],
         tag : {
           type : "白酒",
           isRecommend : true
@@ -63,4 +63,26 @@ var Wine = require('../models').Wine;
         purchaseNum : 0
   });
   wine2.save();
-  console.log('Ok!');*/
+
+  wine3 = new Wine({
+        id : "fy20140716008",
+        name : "国窖1573",
+        describe : "50度国窖1573蓝花釉(大师装)573ml",
+        marketPrice : 980,
+        wechatPrice : 880,
+        littlePic : "fy20140716008_s.jpg",
+        details : {
+          degree : 50,
+          volume : 573,
+          place : '阜阳'
+        },
+        bigPic : ["fy20140716008_l_01.jpg","fy20140716008_l_02.jpg"],
+        tag : {
+          type : "白酒",
+          isRecommend : true
+        },
+        visitNum : 0,
+        purchaseNum : 0
+  });
+  wine3.save();
+  console.log('Ok!');
