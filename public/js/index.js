60a4aa4a60892ef054dc8fc525164af2681b3b7b
zw <pb203097@mail.ustc.edu.cn>
2014-08-18 10:59:42 +0800

    change alert

--- public/js/index.js
+++ public/js/index.js
@@ -44,6 +44,29 @@ require([
         // e.preventDefault();
         $(this).tab('show');
     });
+    //=======================my dialog=====================
+    function myDialogAlert($this,content){
+        var self = $this[0] ;
+        var $(".dialoginuse").remove();
+        var $target = $(".dialogTemplate").clone();
+        $target.addClass("dialoginuse");
+        $target.find(".myAlertLable").text(content.title);
+        $target.find(".ui-alert-body p").text(content.body);
+        var option  = $.extend({}, $target.data(), $this.data());
+        var buttons = content.buttons;
+        buttons.forEach(function(b){
+            var button = '<a data-dismiss="dialog" role="button" class="ui-alert-button">'+b.name+'</a>';
+            var $button = $(button);
+            if(b.callback){
+                $button.on('tap',b.callback);
+            }
+            $target.find(".ui-alert-footer").append($button);
+        });
+
+        if ($this.is('a')) e.preventDefault();
+
+        $target.dialog(option, self);        
+    }
 
 	//=======================css fix=======================
 	function autoResize(){
@@ -355,10 +378,28 @@ require([
     });
     $("#complete_cash_pay").on('tap',function(){
         $(this).attr('disabled','disabled');
+        myDialogAlert($(this),{
+            title : "确认下单",
+            body : "确认下单吗 下单后如有问题请与客服联系",
+            buttons : [
+            {
+                name : "确认",
+                callback : function(){
+                    alert("确认");
+                }
+            },
+            {
+                name : "取消",
+                callback : function(){
+                    $("#complete_cash_pay").removeAttr('disabled','disabled');
+                }
+            }
+            ]
+        });
     });
-    $("#finalConfirmButtonCancel").on('tap',function(){
-        $("#complete_cash_pay").removeAttr('disabled','disabled');
-    });
+    // $("#finalConfirmButtonCancel").on('tap',function(){
+    //     $("#complete_cash_pay").removeAttr('disabled','disabled');
+    // });
     function clearCart(){
         localStorage.cart = '{}';
         $(".order-load-info p").text("购物车中没有商品");
